22599cf3fb3df772d9801397b8dbe7e6cae1bc2e
Alan Bautista <Alan.Bautista@rinf.tech>
2026-07-16 20:26:27 -0600

    Add project files.

---
+++ QTExample.cpp
@@ -0,0 +1,20 @@
+﻿#include "QTExample.h"
+#include <QApplication>
+#include <QWidget>
+#include <QMainWindow>
+
+#include "ui_Formulario.h"
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+
+	QMainWindow mainWindow;
+
+	Ui::Form ui;
+	ui.setupUi(&mainWindow);
+
+	mainWindow.show();
+
+
+    return app.exec();
+}
